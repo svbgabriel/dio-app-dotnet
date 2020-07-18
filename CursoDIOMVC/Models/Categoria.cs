d71a11f1b42b337f80722dd243b193e81c30bc5c
Gabriel Batista <svbgabriel@gmail.com>
2020-07-18 17:09:55 -0300

    Aula 1 - Introdução ao ASP .NET MVC

---
+++ CursoDIOMVC/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoDIOMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
+        public string Descricao { get; set; }
+        public List<Produto> Produtos { get; set; }
+    }
+}
---
+++ CursoDIOMVC/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoDIOMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=dio;Username=dio;Password=dio");
+        }
+    }
+}
